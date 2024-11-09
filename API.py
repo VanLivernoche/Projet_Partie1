6e66d038bb7e81c63d9d5b47112bbaa51234e1bb
Vanessa Livernoche <vanessa.livernoche.1@ulaval.ca>
2024-11-09 15:08:06 -0500

    Sauvegarde

--- API.py
+++ API.py
@@ -1,14 +1,28 @@
 import requests
 
-URL = 'http://www.ulaval.ca/'
+# récupérer le contenu du serveur
+URL = 'https://pax.ulaval.ca/'
 
-# récupérer le contenu de la ressource
-rep = requests.get(URL)
+#Authentification du joueur
+def authentification(idul, secret):
+    # Nous passons les informations d'authentification au serveur
+    # en utilisant le paramètre nommé «auth».
+    rep = requests.get(URL, auth=(idul, secret))
+    print(rep.text)
 
+#vérification du serveur
 if rep.status_code == 200:
     # la réponse est bonne, afficher son contenu
-    print(rep.text)
+   # print(rep.text)
+   print("Vous avez bien accédé au serveur de PAX")
 
 else:
     # une erreur s'est produite, afficher le message d'erreur
     print(f"Le GET sur {URL} a produit le code d'erreur {rep.status_code}.")
+
+
+
+
+#initialiser_partie 
+
+#jouer_un_coup
---
+++ Quixo.py
@@ -0,0 +1,8 @@
+import API
+
+#idul = input("Entrez votre IDUL")
+#secret = input("Entrez votre jeton personnel")
+
+#Informations d'authentification
+idul = "valiv"
+secret = "b009f928-90fb-42c8-877f-1bb2e450ae1d"
