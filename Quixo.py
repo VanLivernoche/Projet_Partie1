2a9bae70783e7466bcbd91fb1977917d1e72b479
Vanessa Livernoche <vanessa.livernoche.1@ulaval.ca>
2024-11-11 13:09:10 -0500

    Formater le damier done

--- Quixo.py
+++ Quixo.py
@@ -1,9 +1,53 @@
 #import API
 
-liste_joueurs = ["valiv", "robot"]
+joueurs = ["valiv", "robot"]
+état_jeu = " "
+plateau = [[" ", " ", "X", " ", " "],
+        [" ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", "O"],
+        [" ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " "],]
+état_plateau = {"joueurs": joueurs, "plateau": plateau}
 
-#formater_entête
+#Fonction formater_entête (3 de 7)
 def formater_entête(joueur1, joueur2):
     print("Légende:\n","X="+joueur1,"\n","O="+joueur2)
 
-formater_entête(liste_joueurs[0], liste_joueurs[1])
+formater_entête(joueurs[0], joueurs[1])
+
+#Fonction formater_le_damier (4 de 7)
+def formater_le_damier(état_plateau):
+    plateau = état_plateau.get("plateau")
+    nb = 1
+    newligne = ""
+    plateau_ascii = "   -------------------\n"
+
+    for ligne in plateau:
+          newligne = " | ".join(str(case) for case in ligne)
+        
+          if nb <= 4:
+            plateau_ascii += str(nb) + " | " + newligne + " |\n"
+            plateau_ascii += "  |---|---|---|---|---|\n"
+            nb += 1
+          else: 
+             plateau_ascii += str(nb) + " | " + newligne + " |\n"
+             plateau_ascii += "--|---|---|---|---|---|\n" + "  | 1   2   3   4   5 |"
+              
+    return plateau_ascii
+formater_le_damier(état_plateau)
+print(formater_le_damier(état_plateau))
+
+
+
+
+
+
+
+#Fonction formater_le_jeu (5 de 7)
+
+
+#Fonction choisir_un_coup (6 de 7)
+
+
+
+#Fonction interpréter_la_commande (7 de 7)
